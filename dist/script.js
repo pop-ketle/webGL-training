a10c9e0247c5377ce23020ab4e29bc742f5d2489
pop-ketle <popqjim@gmail.com>
2020-03-31 19:13:15 +0900

    change img path

--- dist/script.js
+++ dist/script.js
@@ -48,9 +48,9 @@ void main(){
 
 // デモで使用する画像URL
 const assetUrls = [
-    'https://pop-ketle.github.io/portfolio/blob/master/images/img1.jpg',
-    'https://pop-ketle.github.io/portfolio/blob/master/images/img2.jpg',
-    'https://pop-ketle.github.io/portfolio/blob/master/images/dist.jpg'
+    'https://pop-ketle.github.io/portfolio/master/images/img1.jpg',
+    'https://pop-ketle.github.io/portfolio/master/images/img2.jpg',
+    'https://pop-ketle.github.io/portfolio/master/images/dist.jpg'
 ];
 
 // レンダラーの初期化
